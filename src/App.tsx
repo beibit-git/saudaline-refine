3cacaada3abbcbd87939e2c7c6d670a7a960b10d
BEIBIT <beibit.satov@astanait.edu.kz>
2023-03-17 18:19:04 +0600

    inital

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,75 @@
-import { Refine, GitHubBanner, } from '@refinedev/core';
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { Refine } from "@refinedev/core";
+import { Layout, notificationProvider, ErrorComponent } from "@refinedev/antd";
+import routerBindings, {
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { AntdInferencer } from "@refinedev/inferencer/antd";
 
-import { notificationProvider
-,Layout
-,ErrorComponent
-,WelcomePage} from '@refinedev/antd';
 import "@refinedev/antd/dist/reset.css";
 
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier } from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { Header } from "./components/header";
-
-
-
-
-
-function App() {
-    
-
-    
-    
-    return (
-        <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            <ColorModeContextProvider>
-            <Refine notificationProvider={notificationProvider}
-routerProvider={routerBindings}
-dataProvider={dataProvider("https://api.fake-rest.refine.dev")} 
-                options={{
-                    syncWithLocation: true,
-                    warnWhenUnsavedChanges: true,
-                }}
-            >
-                <WelcomePage />
-
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-            </Refine>
-            </ColorModeContextProvider>
-        </RefineKbarProvider>
-        </BrowserRouter>
-      );
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Refine
+        routerProvider={routerBindings}
+        dataProvider={dataProvider("http://localhost:8080/api/v1")}
+        notificationProvider={notificationProvider}
+        resources={[
+          {
+            name: "region",
+            list: "/region",
+            show: "/region/show/:id",
+            create: "/region/create",
+            edit: "/region/edit/:id",
+            meta: {
+              canDelete: true,
+            },
+          },
+          {
+            name: "categories",
+            list: "/categories",
+            show: "/categories/show/:id",
+            create: "/categories/create",
+            edit: "/categories/edit/:id",
+            meta: {
+              canDelete: true,
+            },
+          },
+        ]}
+        options={{
+          syncWithLocation: true,
+          warnWhenUnsavedChanges: true,
+        }}
+      >
+        <Routes>
+          <Route
+            element={
+              <Layout>
+                <Outlet />
+              </Layout>
+            }
+          >
+            <Route path="region">
+              <Route index element={<AntdInferencer />} />
+              <Route path="show/:id" element={<AntdInferencer />} />
+              <Route path="edit/:id" element={<AntdInferencer />} />
+              <Route path="create" element={<AntdInferencer />} />
+            </Route>
+            <Route path="categories">
+              <Route index element={<AntdInferencer />} />
+              <Route path="show/:id" element={<AntdInferencer />} />
+              <Route path="edit/:id" element={<AntdInferencer />} />
+              <Route path="create" element={<AntdInferencer />} />
+            </Route>
+            <Route path="*" element={<ErrorComponent />} />
+          </Route>
+        </Routes>
+        <UnsavedChangesNotifier />
+      </Refine>
+    </BrowserRouter>
+  );
 };
 
 export default App;
